86c0e9b60d88f726cdb1170d4ee40ca3a017fd29
Manu Tarus <manutarus@gmail.com>
2019-12-01 22:03:19 +0300

    add register main condition

--- opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/contract/BaseFpRegisterFragmentContract.java
+++ opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/contract/BaseFpRegisterFragmentContract.java
@@ -29,6 +29,11 @@ public interface BaseFpRegisterFragmentContract {
 
         String getDefaultSortQuery();
 
+        String getDueFilterCondition();
+
+        String getMainTable();
+
+
     }
 
     interface Model {
