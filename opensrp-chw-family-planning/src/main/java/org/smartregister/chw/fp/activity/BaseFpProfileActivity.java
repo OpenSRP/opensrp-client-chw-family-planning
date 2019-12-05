8f9e4fe9345ed22e48af63ea378d0f89eb1bb082
Allan O <a.onchuru@gmail.com>
2019-12-05 12:34:03 +0300

    :rotating_light: Remove unused import

--- opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/activity/BaseFpProfileActivity.java
+++ opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/activity/BaseFpProfileActivity.java
@@ -2,7 +2,6 @@ package org.smartregister.chw.fp.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
