ab1c23cdf49c7b0e8777484b82a4ebaf776ad384
paulinembabu <pauline.mbabu@gmail.com>
2019-12-18 13:15:10 +0300

    Makes modiffications on the query to get the fp details and the FpAlertObject fp start date

--- opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/domain/FpAlertObject.java
+++ opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/domain/FpAlertObject.java
@@ -1,37 +1,34 @@
 package org.smartregister.chw.fp.domain;
-import java.util.Date;
 
 public class FpAlertObject {
     private String fpMethod;
     private Integer fpPillCycles;
-    private Date fpStartDate;
+    private String fpStartDate;
 
-    public FpAlertObject(String fpMethod, Integer fpPillCycles, Date fpStartDate){
-        this.fpMethod = fpMethod;
-        this.fpPillCycles = fpPillCycles;
-        this.fpStartDate = fpStartDate;
+    public FpAlertObject() {
     }
 
     public String getFpMethod() {
         return fpMethod;
     }
 
-    public Integer getFpPillCycles() {
-        return fpPillCycles;
-    }
-
-    public Date getFpStartDate() {
-        return fpStartDate;
-    }
     public void setFpMethod(String fpMethod) {
         this.fpMethod = fpMethod;
+    }
+
+    public Integer getFpPillCycles() {
+        return fpPillCycles;
     }
 
     public void setFpPillCycles(Integer fpPillCycles) {
         this.fpPillCycles = fpPillCycles;
     }
 
-    public void setFpStartDate(Date fpStartDate) {
+    public String getFpStartDate() {
+        return fpStartDate;
+    }
+
+    public void setFpStartDate(String fpStartDate) {
         this.fpStartDate = fpStartDate;
     }
 }
