1e836e6eaf31746f2f0a3ebd5fd4d4e3dad31880
Manu Tarus <manutarus@gmail.com>
2019-12-19 20:39:33 +0300

    add edit form constants

--- opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/util/FamilyPlanningConstants.java
+++ opensrp-chw-family-planning/src/main/java/org/smartregister/chw/fp/util/FamilyPlanningConstants.java
@@ -70,8 +70,10 @@ public interface FamilyPlanningConstants {
         String ACTION = "ACTION";
         String FP_FORM_NAME = "FP_FORM_NAME";
         String REGISTRATION_PAYLOAD_TYPE = "REGISTRATION";
+        String UPDATE_REGISTRATION_PAYLOAD_TYPE = "UPDATE_REGISTRATION";
         String CHANGE_METHOD_PAYLOAD_TYPE = "CHANGE_METHOD";
         String DOB = "DOB";
+        String FORM_AS_STRING = "FORM_AS_STRING";
     }
 
 }
